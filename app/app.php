c3d244ea931ba8b16550413c0cfb39a6a226a7da
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-30 12:58:19 -0700

    add method to capture contacts and store on homepage/ add bootstrap link to header in twig pages

--- app/app.php
+++ app/app.php
@@ -12,11 +12,11 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
     ));
-
-    $app->get("/", function() use ($app) {
-        return $app['twig']->render('contact_form.html.twig');
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('contact_form.html.twig', array('contacts' => Contact::getAll()));
     });
 
+    var_dump(Contact::getAll());
     $app->post('/create_contact', function () use ($app) {
         $contact = new Contact($_POST['first_name'], $_POST['last_name'], $_POST['phone'], $_POST['address']);
         $contact->save();
@@ -24,10 +24,14 @@
             return $app['twig']->render('create_contact.html.twig', array('contacts' => $contact));
     });
 
+    $app->get('/', function() use($app) {
+        return $app['twig']->render('contact_form.html.twig', array('contacts' => Contact::getAll()));
+    });
+
     $app->get('/delete_contacts', function() use ($app) {
         Contact::deleteAll();
         return $app['twig']->render('delete_contacts.html.twig');
     });
-    
+
     return $app;
    ?>
