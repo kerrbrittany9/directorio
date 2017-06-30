8714d4117f85d9c115a89c85aa7128ca954b0f82
Brittany Kerr <kerrbrittany9@gmail.com>
2017-06-30 08:58:59 -0700

    create getters and setters for each property

--- src/contact.php
+++ src/contact.php
@@ -5,17 +5,56 @@
         private $last_name;
         private $phone;
         private $address;
-        private $email;
 
-        function __construct ($first_name, $last_name, $phone, $address, $email)
+        function __construct ($first_name, $last_name, $phone, $address)
 
         {
             $this->first_name = $first_name;
             $this->last_name = $last_name;
             $this->phone = $phone;
             $this->address = $address;
-            $this->email = $email;
         }
+
+        function setFirstName($new_first_name)
+        {
+            $this->first_name = (string) $new_first_name;
+        }
+        function setFirstName($new_last_name)
+        {
+            $this->last_name = (string) $new_last_name;
+        }
+
+        function setFirstName($new_phone)
+        {
+            $this->phone = (string) $new_phone;
+        }
+
+        function setFirstName($new_address)
+        {
+            $this->address = (string) $new_address;
+        }
+
+        function getFirstName()
+        {
+            return $this->first_name
+        }
+
+        function getLastName()
+        {
+            return $this->last_name;
+        }
+
+        function getPhone()
+        {
+            return $this->phone;
+        }
+
+        function getAddress()
+        {
+            return $this->address;
+        }
+
+
 
     }
 
